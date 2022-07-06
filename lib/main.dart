4ef462ad2214238fbaf1eccfb0517c0575a83cbd
Junhyeok Shin <wnsgur1503@naver.com>
2022-07-06 18:29:09 +0900

    Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutterwork/pages/recipe_dart.dart';
+import 'package:flutterwork/components/profile_drawer.dart';
+import 'package:flutterwork/components/profile_header.dart';
+import 'package:flutterwork/components/profile_count_info.dart';
+import 'package:flutterwork/components/profile_tab.dart';
+import 'package:flutterwork/components/profile_buttons.dart';
+import 'package:flutterwork/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +15,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: "PatuaOne"),
-      home: RecipePage(),
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: ProfileDrawer(),
+      appBar: _buildProfileAppBar(),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          ProfileHeader(),
+          SizedBox(height: 20),
+          ProfileCountInfo(),
+          SizedBox(height: 20),
+          ProfileButtons(),
+          Expanded(child: ProfileTab()),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildProfileAppBar() {
+    return AppBar(
+      leading: Icon(Icons.arrow_back_ios),
+      title: Text("Profile", style: TextStyle(color: Colors.black)),
+      centerTitle: true,
     );
   }
 }
