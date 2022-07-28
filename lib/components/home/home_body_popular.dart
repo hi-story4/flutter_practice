2ece2707639ad8a3c66c82387b246570dd645439
Junhyeok Shin <wnsgur1503@naver.com>
2022-07-28 19:37:50 +0900

    yanolja

---
+++ lib/components/home/home_body_popular.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/components/home/home_body_poplar_item.dart';
+import 'package:flutter_practice/size.dart';
+
+class HomeBodyPopular extends StatelessWidget {
+  const HomeBodyPopular({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: md),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildPopularTitle(),
+          _buildPopularList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPopularTitle() {
+    return SizedBox();
+  }
+
+  Widget _buildPopularList() {
+    return Wrap(
+      children: [
+        HomeBodyPopularItem(id: 0),
+        SizedBox(width: 7.5),
+        HomeBodyPopularItem(id: 1),
+        SizedBox(width: 7.5),
+        HomeBodyPopularItem(id: 2),
+      ],
+    );
+  }
+}
